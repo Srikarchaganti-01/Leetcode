81c5c43c20c3bd21dfbe72812b0af94b88c34208
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-03-25 13:38:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,7 +11,7 @@ public:
             int mid = (high-low)/2 + low;
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
             else if ((mid%2 == 1 && nums[mid-1] == nums[mid]) || (mid%2 == 0 && nums[mid] == nums[mid+1])) low  = mid+1;
-            else high = mid-1;
+            else if ((mid%2 == 0 && nums[mid-1] == nums[mid]) || (mid%2 == 1 && nums[mid] == nums[mid+1])) high = mid-1;
         }
         return -1;
     }
