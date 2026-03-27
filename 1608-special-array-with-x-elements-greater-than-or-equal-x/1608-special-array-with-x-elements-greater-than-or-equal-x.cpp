1a8f4bf079bd55147010a0bfe5b1877a69a81926
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-03-27 22:51:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        for(int i=1;i<=n;i++){
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int mid = (low+high)/2;
             int count = 0;
             for(int j =0;j<n;j++){
-                if(nums[j] >= i) count++;
+                if(nums[j] >= mid) count++;
             }
-            if(count == i) return i;
+            if(count == mid) return mid;
+            else if(count > mid) low = mid+1;
+            else if(count < mid) high = mid-1;
             count = 0;
         }
         return -1;
