a2667a8d8f86d597a8fbd7e47e0b259116cb4e3d
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-03 23:55:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,24 +11,13 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int pos = 0;
-        while(temp!=nullptr){
-            pos++;
-            temp = temp->next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast!= nullptr && 
+        fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        int curr = 0;
-        int lim = ceil(pos/2);
-        temp = head;
-        while(temp!=nullptr){
-            if(curr < lim) {
-                curr++;
-                temp = temp->next;
-                cout<<curr<<" ";
-                continue;
-            }
-            return temp;
-        }
-        return head;
+        return slow;
     }
 };
