d14ef6db334a3cc3c68e6b18f2ac0614f0a5353b
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-03 18:48:56 +0530

    Time: 3 ms (99.59%), Space: 11.9 MB (22.08%) - LeetHub

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,12 +10,17 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         int impostor = INT_MIN;
+        int pos = 0;
         ListNode* temp = head;
         if(head == nullptr) return false;
         while(temp->next != nullptr){
-            if(temp->val == impostor) return true;
+            if(temp->val == impostor){
+                cout<<pos;
+                return true;
+            }
             temp->val = impostor;
             temp = temp->next;
+            pos++;
         }
         return false;
     }
