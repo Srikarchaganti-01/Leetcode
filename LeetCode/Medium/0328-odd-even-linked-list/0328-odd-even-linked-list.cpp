d7d7cd4fcbb1b863c26c07214a93d2dd1557e96f
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-04-04 22:56:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ LeetCode/Medium/0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,30 +11,17 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        int pos = 1;
-        ListNode* temp = head;
-        queue<int>q1;
-        queue<int>q2;
-        while(temp != nullptr){
-            if(pos %2 == 0){
-                q1.push(temp->val);
-            }
-            else{
-                q2.push(temp->val);
-            }
-            pos++;
-            temp = temp->next;
+        if (!head || !head->next || !head->next->next) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evhead = even; 
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        temp = head;
-        while(!q2.empty()){
-            temp->val = q2.front();
-            q2.pop();
-            temp = temp->next;
-        }
+        odd->next = evhead;
         return head;
     }
 };
