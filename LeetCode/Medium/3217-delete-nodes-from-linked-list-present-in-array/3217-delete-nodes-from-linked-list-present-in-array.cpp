143cfd80c9ec48e22661b2b898c309a41c845a95
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-06 22:44:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
+++ LeetCode/Medium/3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -10,23 +10,13 @@
  */
 class Solution {
 public:
-    bool searchvector(vector<int>& nums,int target){
-        int low = 0;
-        int high = nums.size()-1;
-        while(low<=high){
-            int mid = (low+high) / 2;
-            if(nums[mid] == target) return true;
-            else if(nums[mid] > target) high = mid-1;
-            else if(nums[mid] < target) low = mid+1;
-        }
-        return false;
-    }
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         ListNode* temp = head;
         queue<int>q;
-        sort(nums.begin(),nums.end());
+        unordered_set<int> st(nums.begin(), nums.end());
         while(temp!=nullptr){
-            if(searchvector(nums,temp->val)){
+            if(st.count(temp->val))
+            {
                 temp = temp->next;
                 continue;
             }
