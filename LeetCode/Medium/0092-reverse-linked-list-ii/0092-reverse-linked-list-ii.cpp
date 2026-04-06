2f8865c289016a59d49152bf287bfa7cc00c0d86
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-06 19:32:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -13,22 +13,17 @@ public:
     ListNode* rev(ListNode* newhead,int left,int right,int ptr){
         ListNode* temp = newhead;
         stack<int>st;
-        while(temp!=nullptr && ptr <= right){
+        while(temp != nullptr && ptr <= right){
             st.push(temp->val);
             temp = temp->next;
             ptr++;
         }
-        ListNode* nextnode;
-        if(temp == nullptr) nextnode = nullptr;
-        if(temp != nullptr) nextnode = temp->next;
-        
         temp = newhead;
         while(!st.empty()){
             temp->val = st.top();
             st.pop();
             temp = temp->next;
         }
-        if(nextnode) temp->next = nextnode;
         return newhead;
     }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
