2ca3740e5c43429aed11b2350352d0e7fed1f987
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-04-19 11:53:48 +0530

    Time: 0 ms (100%), Space: 12.1 MB (40.45%) - LeetHub

---
+++ LeetCode/Easy/2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int maxi = INT_MIN;
+        for(int i=-0;i<colors.size();i++){
+            for(int j=i;j<colors.size();j++){
+                if(colors[i] != colors[j]) maxi = max(maxi, abs(j-i));
+            }
+        }
+        return maxi;
+    }
+};
