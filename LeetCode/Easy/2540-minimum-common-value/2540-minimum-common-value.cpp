db56985558f9ed31be56e07f71ce8d2c0f240e3d
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-05-19 11:39:05 +0530

    Time: 0 ms (100%), Space: 54.5 MB (58.81%) - LeetHub

--- LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.cpp
+++ LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        for(int i=0;i<nums1.size();i++){
-            int low = 0;
-            int high = nums2.size()-1;
-            while(low<=high){
-                int mid = (low+high)/2;
-                if(nums1[i] == nums2[mid]) return nums2[mid];
-                else if(nums1[i] > nums2[mid])low = mid+1;
-                else if(nums1[i] < nums2[mid])high = mid-1;
-            }
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] == nums2[j]) return nums1[i];
+            else if(nums1[i] > nums2[j]) j++;
+            else i++;
         }
         return -1;
     }
