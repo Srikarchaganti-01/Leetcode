929cd19e80a1c116330c39e0b8d0103c0cc0ebfd
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-05-22 06:21:49 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.43%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]] = i;
-        }
-        sort(nums.begin(),nums.end());
-        for(auto it:mp){
-            if(it.first == target) return it.second;
+            if(nums[i] == target) return i;
         }
         return -1;
     }
