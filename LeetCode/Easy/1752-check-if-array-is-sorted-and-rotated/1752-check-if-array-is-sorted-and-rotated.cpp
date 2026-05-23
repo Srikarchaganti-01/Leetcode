4ba14672056e7332199c0252938c3910853c6666
Srikar Chaganti <srikarchaganti134@gmail.com>
2026-05-23 09:04:30 +0530

    Time: 0 ms (100%), Space: 11.9 MB (5.2%) - LeetHub

--- LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         vector<int>arr = nums;
-        vector<int>id = nums;
-        sort(id.begin(),id.end());
-        for(int i=0;i<=nums.size();i++){
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
             rotate(arr.begin(), arr.begin() + 1, arr.end());
-            if((arr == id)) return true;
+            if(arr == nums) return true;
         }
         return false;
     }
